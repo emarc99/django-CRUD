5e349b2c29ec8c5a410866c8185b2b7ae13533be
Emmanuel <emmanueladedejiaroso@gmail.com>
2022-06-21 10:41:34 +0100

    Completed task

---
+++ I4G0012341J3/blog/views.py
@@ -0,0 +1,29 @@
+from pyexpat import model
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from .models import Post
+
+
+# Create your views here.
+class PostListView(ListView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
